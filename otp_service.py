11c6ee36557ba3154ef329202e7259af8b7af189
Hansika <hs9206011@gmail.com>
2026-04-06 23:50:12 +0530

    Delete otp_service.py

--- otp_service.py
+++
@@ -1,9 +0,0 @@
-otp_store = {}
-
-def send_otp(email):
-    otp = "123456"  # simulate
-    otp_store[email] = otp
-    return {"message": "OTP sent", "otp": otp}
-
-def verify_otp(email, otp):
-    return otp_store.get(email) == otp
